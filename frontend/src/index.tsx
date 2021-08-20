3878409ba612fe24cc57f5847ef4687e5d02d0e7
tandrew468 <tandrew468@gmail.com>
2021-08-20 17:43:52 +1000

    Add ability to send data to database via register
    * Refactoring needed for Register
    
    * Added Axios package 
    
    * Refactored index and login

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,34 +1,34 @@
-import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import {CssBaseline, ThemeProvider} from "@material-ui/core";
 import React from "react";
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./pages/App";
 import reportWebVitals from "./reportWebVitals";
 import theme from "./theme";
 import Login from "./pages/Login/Login";
-import Register from "./pages/SignUp/Register";
+import Register from "./pages/Register/Register";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <CssBaseline />
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/app">
-            <App />
-          </Route>
-            <Route path="/register" >
-                <Register />
-            </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Redirect to="/login" />
-        </Switch>
-      </BrowserRouter>
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <CssBaseline/>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/app">
+                        <App/>
+                    </Route>
+                    <Route path="/api/users/register">
+                        <Register/>
+                    </Route>
+                    <Route path="/api/users/login">
+                        <Login/>
+                    </Route>
+                    <Redirect to="/login"/>
+                </Switch>
+            </BrowserRouter>
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
