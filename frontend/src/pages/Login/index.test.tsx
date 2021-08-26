efd1124d756e312978bd8eb4ba1f6f1a3841a640
Ryan <ryan2002.rs@gmail.com>
2021-08-26 12:12:28 +1000

    Update tests

--- frontend/src/pages/Login/index.test.tsx
+++ frontend/src/pages/Login/index.test.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Login from ".";
 
 test("renders learn react link", () => {
-  const { container } = render(<Login />);
+  const { container } = render(
+    <QueryClientProvider client={new QueryClient()}>
+      <Login />
+    </QueryClientProvider>
+  );
   expect(container).toMatchSnapshot();
 });
--- frontend/src/pages/Register/index.test.tsx
+++ frontend/src/pages/Register/index.test.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Register from ".";
 
 test("renders register page", () => {
-  const { container } = render(<Register />);
+  const { container } = render(
+    <QueryClientProvider client={new QueryClient()}>
+      <Register />
+    </QueryClientProvider>
+  );
   expect(container).toMatchSnapshot();
 });
