a23f1d58c1a494ca291159f5404b46ca1a2d650f
Ryan <ryan2002.rs@gmail.com>
2021-08-26 18:03:44 +1000

    Fix tests

--- frontend/src/pages/Register/index.test.tsx
+++ frontend/src/pages/Register/index.test.tsx
@@ -3,6 +3,10 @@ import { render } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Register from ".";
 
+jest.mock("../../util/Link", () => "mock-Link");
+jest.mock("../../util/LoadingButton", () => "mock-LoadingButton");
+jest.mock("../../util/FormField", () => "mock-FormField");
+
 test("renders register page", () => {
   const { container } = render(
     <QueryClientProvider client={new QueryClient()}>
--- frontend/src/util/Link/index.test.tsx
+++ frontend/src/util/Link/index.test.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import Link from ".";
 
 test("renders internal link", () => {
-  const { container } = render(<Link to="/example-page">Example</Link>);
+  const { container } = render(
+    <BrowserRouter>
+      <Link to="/example-page">Example</Link>
+    </BrowserRouter>
+  );
   expect(container).toMatchSnapshot();
 });
