bbd93d77b5bdc3c7254b6311570d886fe5ec15e7
Ryan <ryan2002.rs@gmail.com>
2021-09-16 22:23:20 +1000

    Work on auto-login

--- frontend/src/components/GlobalContext.tsx
+++ frontend/src/components/GlobalContext.tsx
@@ -1,9 +1,17 @@
 import React, { createContext, FC, useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { api, TokenProps } from "../util/api";
 
-interface User {
-  // TODO
-  x: number;
+export interface User {
+  createdAt: string;
+  enabled: boolean;
+  firstName: string;
+  id: number;
+  lastName: string;
+  password: string;
+  roles: "ROLE_USER" | "ROLE_ADMIN";
+  updatedAt: string | null;
+  username: string;
 }
 
 interface GlobalContextType {
@@ -17,17 +25,19 @@ export const GlobalContext = createContext<GlobalContextType>({} as never);
 export const GlobalContextProvider: FC<unknown> = ({ children }) => {
   const [user, setUser] = useState<User>();
   const [token, setToken] = useState<string>();
+  const getUser = useQuery("user");
   useEffect(() => {
     api.defaults.headers.Authorization = localStorage.getItem("token");
   }, [token]);
+  useEffect(() => {});
 
   return (
     <GlobalContext.Provider
       value={{
         user,
         login: (data) => {
-          localStorage.setItem("token", data.token as string);
-          setToken(data.token);
+          localStorage.setItem("token", data.jwt as string);
+          setToken(data.jwt);
         },
       }}
     >
