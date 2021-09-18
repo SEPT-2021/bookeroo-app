7e36c4887a166043e0d2f057c27597c9f5a0b358
tandrew468 <tandrew468@gmail.com>
2021-09-18 23:55:45 +1000

    Added Test for PageNotFound

---
+++ frontend/src/pages/NotFoundPage/index.test.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { render, cleanup } from "@testing-library/react";
+import { Button } from "@material-ui/core";
+import { QueryClient, QueryClientProvider } from "react-query";
+import NotFoundPage from "./index";
+
+afterEach(cleanup);
+describe("Rendering page NotFoundPage", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<NotFoundPage />, div);
+    expect(div).toMatchSnapshot();
+  });
+});
+
+describe("Button render matches snapshot", () => {
+  test("renders button", () => {
+    const button = render(<Button />);
+    expect(button).toMatchSnapshot();
+  });
+});
+
+describe("NotFoundPage matches snapshot", () => {
+  test("render snapshot", () => {
+    const { container } = render(
+      <QueryClientProvider client={new QueryClient()}>
+        <NotFoundPage />
+      </QueryClientProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
