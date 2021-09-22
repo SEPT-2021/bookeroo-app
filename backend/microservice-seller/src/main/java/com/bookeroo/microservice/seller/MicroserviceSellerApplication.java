e54456fa802a55b4dff6c9ab752df662584455fe
Shihab Sami <shihabsami14120@gmail.com>
2021-09-23 00:31:09 +1000

    Initial commit

---
+++ backend/microservice-seller/src/main/java/com/bookeroo/microservice/seller/MicroserviceSellerApplication.java
@@ -0,0 +1,13 @@
+package com.bookeroo.microservice.seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSellerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceSellerApplication.class, args);
+    }
+
+}
