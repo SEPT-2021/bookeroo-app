dcda9ecf22eabeab2e1da00a7541d7b6e0207ce5
tandrew468 <tandrew468@gmail.com>
2021-10-27 08:31:56 +1100

    Updated view listings page in user details

--- frontend/src/pages/ViewListings/index.tsx
+++ frontend/src/pages/ViewListings/index.tsx
@@ -1,23 +1,59 @@
 import * as React from "react";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import { useQuery } from "react-query";
-import { viewListings } from "../../util/api";
+import { DataGrid, GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { Delete } from "@material-ui/icons";
+import {
+  CircularProgress,
+  createStyles,
+  IconButton,
+  withStyles,
+  WithStyles,
+} from "@material-ui/core";
+import { deleteListingById, viewListings } from "../../util/api";
+import { snakeCaseToNormalString } from "../../util/string-util";
+import theme from "../../theme";
 
-const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "price", headerName: "Price", width: 130 },
-  { field: "bookCondition", headerName: "Book Condition", width: 130 },
-];
-
-export default function ViewListings() {
+function ViewListings({ classes }: ViewListingsProps) {
   const { data } = useQuery("orders", viewListings);
 
-  console.log(data);
+  const rows = data || [];
 
-  const rows = [{ id: 1, price: 11, bookCondition: "NEW" }];
+  const client = useQueryClient();
+  const { mutate: deleteMutate, isLoading } = useMutation(deleteListingById, {
+    onSuccess: () => {
+      client.invalidateQueries("orders");
+    },
+  });
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 90 },
+    { field: "price", headerName: "Price", width: 110 },
+    { field: "userFullName", headerName: "Full Name", width: 250 },
+    {
+      field: "bookCondition",
+      headerName: "Book Condition",
+      width: 175,
+      valueFormatter: ({ value }) =>
+        typeof value === "string" ? snakeCaseToNormalString(value) : value,
+    },
+    {
+      field: "Delete Listing",
+      headerName: "Delete Listing",
+      width: 175,
+      renderCell: (val: GridRenderCellParams) => (
+        <IconButton onClick={() => deleteMutate({ id: val.row.id })}>
+          {isLoading ? (
+            <CircularProgress color="secondary" size={20} />
+          ) : (
+            <Delete />
+          )}
+        </IconButton>
+      ),
+    },
+  ];
 
   return (
-    <div style={{ height: 400, width: "350%" }}>
+    <div style={{ height: 400, width: "450%" }} className={classes.root}>
       <DataGrid
         rows={rows}
         columns={columns}
@@ -28,3 +64,20 @@ export default function ViewListings() {
     </div>
   );
 }
+
+const styles = () =>
+  createStyles({
+    root: {
+      height: "30vh",
+    },
+    box: {
+      "& > *": {
+        margin: theme.spacing(1),
+        width: "25ch",
+      },
+    },
+  });
+
+type ViewListingsProps = WithStyles<typeof styles>;
+
+export default withStyles(styles)(ViewListings);
