5f324ccfbad4886b5d3a5245909d6ad33348c446
Ryan <ryan2002.rs@gmail.com>
2021-10-28 23:07:03 +1100

    Fix CI

--- frontend/src/pages/CheckOut/index.test.tsx
+++ frontend/src/pages/CheckOut/index.test.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {cleanup, render} from "@testing-library/react";
-import {QueryClient, QueryClientProvider} from "react-query";
-import {BrowserRouter} from "react-router-dom";
+import { cleanup, render } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from "react-router-dom";
 import CheckOut from "./index";
 import LoadingButton from "../../util/LoadingButton";
-import {GlobalContextProvider} from "../../components/GlobalContext";
+import { GlobalContextProvider } from "../../components/GlobalContext";
 
 afterEach(cleanup);
 describe("Rendering page CheckOut", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(
-        <BrowserRouter>
-          <QueryClientProvider client={new QueryClient()}>
-            <GlobalContextProvider>
-              <CheckOut/>
-            </GlobalContextProvider>
-          </QueryClientProvider>
-        </BrowserRouter>,
-        div
+      <BrowserRouter>
+        <QueryClientProvider client={new QueryClient()}>
+          <GlobalContextProvider>
+            <CheckOut />
+          </GlobalContextProvider>
+        </QueryClientProvider>
+      </BrowserRouter>,
+      div
     );
     expect(div).toMatchSnapshot();
   });
